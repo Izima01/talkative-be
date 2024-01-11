6960d13776dbecef1694b13c57b365419802c7e7
Izima Obisike <kingsleyizima@gmail.com>
2024-01-11 13:12:41 +0100

    Update users.ts

--- src/models/users.ts
+++ src/models/users.ts
@@ -13,10 +13,6 @@ export const getUsers = () => UserModel.find();
 
 export const getUserByUsername = (username: string) => UserModel.findOne({ username });
 
-export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({ 'authentication.sessionToken': sessionToken });
-
 export const getUserById = (id: string) => UserModel.findById(id);
-
-export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
 
 export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values, { new: true });
