1454a833877b78a72f2988718e543da4bcfce3f6
izima01 <kingsleyizima@gmail.com>
2024-01-24 20:24:10 +0100

    edited error messages

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -7,7 +7,7 @@ export const notFound = (req: Request, res: Response, next: NextFunction) => {
 };
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  const statusCode = res.status === 200 ? 500 : res.status;
   res.status(statusCode);
   res.json({
     message: err.message,
